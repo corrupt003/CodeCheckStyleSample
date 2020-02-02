c1fe8506832ee547284ee1dc8d5fca6070fff623
corrupt003 <corrupt003@gmail.com>
2020-02-02 17:01:05 +0800

    Add Checkstyle plugin
    1. Add two config files.
    2. Run Checkstyle locally only with modified java file.
    3. Add one test java class file.

---
+++ app/src/main/java/com/corrupt003/codecheckstylesample/TestJavaUtils.java
@@ -0,0 +1,13 @@
+package com.corrupt003.codecheckstylesample;
+
+/**
+ * A test class used to see if checkstyle works or not.<br>
+ * You can modify any line of this class and run the test in `build.gradle`.
+ */
+public class TestJavaUtils {
+    public static int getNumber()
+    {
+        // Should add whitespace before and after `-`.
+        return 2-1;
+    }
+}
